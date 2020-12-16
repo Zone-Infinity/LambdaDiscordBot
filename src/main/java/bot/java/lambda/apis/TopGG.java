dc4469b81ce6ae8de398af6034b1146e133ee05e
Soham Dokhale <sohamdokhale1602@gmail.com>
2020-12-16 13:34:13 +0530

    Added Logger to TopGG posting

--- src/main/java/bot/java/lambda/apis/TopGG.java
+++ src/main/java/bot/java/lambda/apis/TopGG.java
@@ -3,12 +3,15 @@ package bot.java.lambda.apis;
 import bot.java.lambda.config.Config;
 import net.dv8tion.jda.api.JDA;
 import org.discordbots.api.client.DiscordBotListAPI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class TopGG {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TopGG.class);
 
     private static void postServerCount(JDA jda, int serverCount) {
         DiscordBotListAPI api = new DiscordBotListAPI.Builder()
@@ -23,6 +26,7 @@ public class TopGG {
         Runnable postServerCount = () -> {
             final int guildCount = jda.getGuilds().size();
             postServerCount(jda, guildCount);
+            LOGGER.info("Posted Server Count to top.gg");
         };
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
         executor.scheduleWithFixedDelay(postServerCount, 2, delay, TimeUnit.MINUTES);
