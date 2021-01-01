149fa9c48cccffc14c985530162396829f16b34a
Soham Dokhale <sohamdokhale1602@gmail.com>
2021-01-01 10:01:15 +0530

    Removed .gitignore

--- src/main/java/bot/java/lambda/config/Config.java
+++ src/main/java/bot/java/lambda/config/Config.java
@@ -1,13 +1,14 @@
 package bot.java.lambda.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 public class Config {
 
-    private static final Dotenv dotenv = Dotenv.load();
+    //private static final Dotenv dotenv = Dotenv.load();
 
     public static String get(String key) {
-        return dotenv.get(key.toUpperCase());
+        //return dotenv.get(key.toUpperCase());
+        return System.getenv(key);
     }
 
 }
