245f8a2c18548a0b62215e4c1a2df1cff8909178
LoadingBG <troyankru@gmail.com>
2021-05-02 17:42:10 +0300

    Streamify GuildsCommand

--- src/main/java/bot/java/lambda/command/commands/admin/GuildsCommand.java
+++ src/main/java/bot/java/lambda/command/commands/admin/GuildsCommand.java
@@ -6,56 +6,59 @@ import bot.java.lambda.command.type.ICommand;
 import bot.java.lambda.config.Config;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.TextChannel;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class GuildsCommand implements ICommand {
     @Override
     public void handle(CommandContext ctx) {
         if (ctx.getAuthor().getIdLong() == Long.parseLong(Config.get("owner_id"))) {
-            final TextChannel channel = ctx.getChannel();
-            List<Guild> guilds = new ArrayList<>(ctx.getJDA().getGuilds());
-            final List<List<Guild>> guildsList = new ArrayList<>();
+            List<Guild> guilds = ctx.getJDA().getGuilds()
+                .stream()
+                .sorted(Comparator.comparingInt(Guild::getMemberCount).reversed())
+                .collect(Collectors.toList());
+            final List<List<Guild>> guildsList = IntStream.range(0, guilds.size())
+                .boxed()
+                .collect(Collectors.groupingBy(i -> i / 4))
+                .values()
+                .stream()
+                .map(indices -> indices.stream().map(guilds::get).collect(Collectors.toList()))
+                .collect(Collectors.toList());
             try {
-                final int page = Integer.parseInt(ctx.getArgs().get(0));
-
-                guilds.sort(Comparator.comparingInt(Guild::getMemberCount));
-                Collections.reverse(guilds);
-
-                final int size = guilds.size();
-                final int guildsLists = size / 10;
-
-                for (int i = 1; i <= guildsLists; i++) {
-                    int j = i * 10;
-                    guildsList.add(guilds.subList(j - 10, j - 1));
-                }
-
-                final int remainingGuilds = size % 10;
-                final int guildsInTen = guildsLists * 10;
-                guildsList.add(guilds.subList(guildsInTen, guildsInTen + remainingGuilds));
-
-                channel.sendMessage(new EmbedBuilder()
+                final List<String> args = ctx.getArgs();
+                final int page = args.isEmpty() ? 1 : Integer.parseInt(args.get(0));
+                ctx.getChannel().sendMessage(new EmbedBuilder()
                         .setDescription("```" + getGuildTable(guildsList.get(page - 1)) + "```")
                         .build()).queue();
             } catch (NumberFormatException e) {
-                channel.sendMessage("Provide a number").queue();
+                ctx.getChannel().sendMessage("Provide a number").queue();
             } catch (IndexOutOfBoundsException e) {
-                channel.sendMessage("No guilds on the page").queue();
+                ctx.getChannel().sendMessage("No guilds on the page").queue();
             }
         }
     }
 
     private String getGuildTable(List<Guild> guildList) {
         StringBuilder table = new StringBuilder();
+        final int guildSize = guildList.stream()
+            .mapToInt(it -> Math.min(it.getName().length(), 22))
+            .max()
+            .orElse(0);
+        final int memberSize = guildList.stream()
+            .mapToInt(it -> String.valueOf(it.getMemberCount()).length())
+            .max()
+            .orElse(0);
 
-        final int guildSize = guildList.stream().mapToInt(it -> Math.min(it.getName().length(), 22)).max().orElse(0);
-        final int memberSize = guildList.stream().mapToInt(it -> String.valueOf(it.getMemberCount()).length()).max().orElse(0);
-
-        String rowFormat = "║%-" + (Math.max(5, String.valueOf(guildList.size()).length()) + 1) + "s║%-" + (Math.max(guildSize, 5) + 1) + "s║%-" + (Math.max(memberSize, 7) + 1) + "s║%-18s║%n";
+        String rowFormat = "║%-" +
+            (Math.max(5, String.valueOf(guildList.size()).length()) + 1) +
+            "s║%-" +
+            (Math.max(guildSize, 5) + 1) +
+            "s║%-" +
+            (Math.max(memberSize, 7) + 1) +
+            "s║%-18s║%n";
         final String divider = String.format(rowFormat, "", "", "", "").replaceAll(" ", "═");
 
         table.append(String.format(rowFormat, "", "", "", "").replaceFirst("║", "╔").replaceFirst("║", "╦").replaceFirst("║", "╦").replaceFirst("║", "╦").replaceFirst("║", "╗").replaceAll(" ", "═"));
