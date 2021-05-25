a7a16613403e9e01da72586b879bded72ca11c3a
Zone-Infinity <sohamdokhale1602@gmail.com>
2021-05-25 15:08:26 +0530

    Try something

--- src/main/java/bot/java/lambda/utils/StringUtils.java
+++ src/main/java/bot/java/lambda/utils/StringUtils.java
@@ -11,14 +11,16 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StringUtils {
-    private static final String NORMAL_TEXT = "abcdefghijklmnopqrstuvwxyz_,.?!/\\ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&<> ɐqɔpǝɟɓɥᴉɾʞlɯuodbɹsʇnʌʍxʎz‾'˙¿¡/\\ⱯᗺƆᗡƎℲ⅁HIᒋꓘ⅂WNOԀტᴚS⊥∩ΛMX⅄Z0⇂ᘔƐㄣ59ㄥ86⅋><";
-    private static final String FLIPPED_TEXT = "ɐqɔpǝɟɓɥᴉɾʞlɯuodbɹsʇnʌʍxʎz‾'˙¿¡/\\ⱯᗺƆᗡƎℲ⅁HIᒋꓘ⅂WNOԀტᴚS⊥∩ΛMX⅄Z0⇂ᘔƐㄣ59ㄥ86⅋>< abcdefghijklmnopqrstuvwxyz_,.?!/\\ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&<>";
-    private static final Map<Character, Character> LETTER_PAIRS = IntStream.range(0, NORMAL_TEXT.length())
-        .boxed()
-        .collect(Collectors.toMap(NORMAL_TEXT::charAt, FLIPPED_TEXT::charAt));
+    private static final List<Character> NORMAL_TEXT = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ ɐqɔpǝɟɓɥᴉɾʞlɯuodbɹsʇnʌʍxʎz ⱯᗺƆᗡƎℲ⅁HIᒋꓘ⅂WNOԀტᴚS⊥∩ΛMX⅄Z _,.?!/\\‾'˙¿¡/\\ 0⇂ᘔƐㄣ59ㄥ86⅋>< 0123456789&<>".chars().mapToObj(e -> (char) e).collect(Collectors.toSet()).stream().toList();
+    private static final List<Character> FLIPPED_TEXT = "ɐqɔpǝɟɓɥᴉɾʞlɯuodbɹsʇnʌʍxʎz ⱯᗺƆᗡƎℲ⅁HIᒋꓘ⅂WNOԀტᴚS⊥∩ΛMX⅄Z abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ ‾'˙¿¡/\\_,.?!/\\ 0123456789&<> 0⇂ᘔƐㄣ59ㄥ86⅋><".chars().mapToObj(e -> (char) e).collect(Collectors.toSet()).stream().toList();
+
+    private static final Map<Character, Character> LETTER_PAIRS = IntStream.range(0, NORMAL_TEXT.size())
+            .boxed()
+            .collect(Collectors.toMap(NORMAL_TEXT::get, FLIPPED_TEXT::get));
 
     private static final String PROFANITY_URL = "https://raw.githubusercontent.com/RobertJGabriel/Google-profanity-words/master/list.txt";
     private static final List<String> profanityWords = new ArrayList<>();
+
     static {
         try {
             final URL url = new URL(PROFANITY_URL);
@@ -32,13 +34,13 @@ public class StringUtils {
 
     public static String flipText(String text) {
         return text.chars()
-            .mapToObj(p -> {
-                final char c = (char) p;
-                return LETTER_PAIRS.getOrDefault(c, c);
-            })
-            .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
-            .reverse()
-            .toString();
+                .mapToObj(p -> {
+                    final char c = (char) p;
+                    return LETTER_PAIRS.getOrDefault(c, c);
+                })
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .reverse()
+                .toString();
     }
 
     public static boolean hasProfanity(String text) {
